510ff398badd1faf8741d7bcac3d2d240bca0410
Aqeel Labash <aqeel13932@users.noreply.github.com>
2022-06-17 17:55:01 +0300

    Update Add Two Number.py

--- Add Two Number.py
+++ Add Two Number.py
@@ -5,6 +5,7 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        #Way 1
         result = []
         while l1 is not None and l2 is not None:
             result.append(l1.val+l2.val)
@@ -37,3 +38,53 @@ class Solution:
                 current = current.next
 
         return start
+    
+        #Way 2
+            def num2carry(num):
+                return num%10,num//10
+
+            start=ListNode()
+            current = start
+            carry = 0
+            
+            while l1 is not None and l2 is not None:
+
+                current.val = l1.val +l2.val+carry
+                carry=0
+                if current.val>9:
+                    current.val,carry=num2carry(current.val)
+
+                l1 = l1.next
+                l2 = l2.next
+                if l1 is not None and l2 is not None:
+                    current.next = ListNode()
+                    current = current.next
+                    
+            if l1 is not None or l2 is not None:
+                current.next = ListNode()
+                current = current.next
+            while l1 is not None:
+                current.val = l1.val + carry
+                carry = 0
+                if current.val>9:
+                    current.val,carry=num2carry(current.val)
+                l1 = l1.next
+                if l1 is not None:
+                    current.next = ListNode()
+                    current = current.next
+   
+            while l2 is not None:
+                current.val = l2.val + carry
+                carry =0
+                if current.val>9:
+                    current.val,carry=num2carry(current.val)
+                l2 = l2.next
+                if l2 is not None:
+                    current.next = ListNode()
+                    current = current.next
+            
+            if carry>0:
+                current.next =ListNode()
+                current = current.next
+                current.val = carry
+            return start
